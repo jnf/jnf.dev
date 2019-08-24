d6a8ffdb5fa36b8a2bf543ecfb06ad32adb3a4d2
jnf <jnf@has-many.com>
2019-08-24 17:51:26 -0400

    creates a component for licensing page content

---
+++ src/components/license.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const ContentLicense = ({ path = '#', title }) =>
+  <div className='license-content'>
+    <a rel='license' href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
+      <img
+        alt='Creative Commons License'
+        src='https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png'
+      />
+    </a>
+    {title} by
+    <a href='https://jnf.dev'>jnf</a>
+    is licensed under a
+    <a rel='license' href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
+      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+    </a>.
+    <a href={path}>Based on this original work.</a>.
+  </div>
+
+ContentLicense.propTypes = {
+  path: PropTypes.string,
+  title: PropTypes.string.required
+}
