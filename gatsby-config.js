7352b0f9b7895194c017002570380bdf5b549f06
jnf <jnf@has-many.com>
2019-08-24 18:25:48 -0400

    replaces some generic info with some slightly less generic info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'jnf.dev',
+    description: 'a small archive of content from jnf, a Seattle-based software developer and leader',
+    author: '@_jnf',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -18,12 +18,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        name: 'jnf.dev',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
