3c0dc8fd3e27f7cdadc69b3ae6b26a17637af617
jnf <jnf@has-many.com>
2020-01-23 21:52:08 -0800

    updates the layout styles to be scss, hopefully getting us good sass imports

--- src/templates/page.js
+++ src/templates/page.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from '../components/header'
-import '../styles/layout.css'
+import '../styles/layout.scss'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
