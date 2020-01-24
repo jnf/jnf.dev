5d82159103ac0f12db66f86f39bb6144788bc35d
jnf <jnf@has-many.com>
2020-01-23 22:43:38 -0800

    starts adding real(-ish) content to the site index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,67 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 
 import Layout from '../templates/page'
-import Image from '../components/image'
 import Seo from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title='Home' />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to='/page-2/'>Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const talks = allMarkdownRemark.edges.reduce((acc, { node: { frontmatter, fields } }) => ([
+    ...acc,
+    [frontmatter.title, fields.slug]
+  ]), [])
+
+  return (
+    <Layout>
+      <Seo title='jnf.dev: where jnf talks about dev' />
+      <article className='static-page'>
+        <section>
+          <h2>Hello</h2>
+          <p>
+            There's not much to see right now.
+          </p>
+        </section>
+        <aside>
+          <h3>Table of Contents</h3>
+          <nav>
+            <h4>Pages</h4>
+            <ul>
+              <li><Link to='/' replace>Home</Link></li>
+            </ul>
+          </nav>
+          <nav>
+            <h4>Presentations</h4>
+            <ul>
+              {talks.map(([title, path]) => <li key={path}><Link to={path} replace>{title}</Link></li>)}
+            </ul>
+          </nav>
+          <nav>
+            <h4>External Links</h4>
+            <ul>
+              <li><a href='https://twitter.com/_jnf'>@_jnf</a></li>
+              <li><a href='https://www.linkedin.com/in/jeremyf/'>LinkedIn</a></li>
+            </ul>
+          </nav>
+        </aside>
+      </article>
+    </Layout>
+  )
+}
 
 export default IndexPage
