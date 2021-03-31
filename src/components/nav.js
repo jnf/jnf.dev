512ff3e3c3efcfef82122d8865140dc54447e088
jnf <code@jnf.dev>
2021-03-31 08:11:09 -0700

    removes old connect links from main nav, gets the dom structure grid-ready

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 
-import twitterIcon from '../images/twitter.svg'
-import githubIcon from '../images/github.svg'
-import linkedinIcon from '../images/linkedin.svg'
-import discogsIcon from '../images/discogs.svg'
-
 export const Navigation = () => {
   const { allMdx } = useStaticQuery(graphql`
     {
@@ -41,7 +36,7 @@ export const Navigation = () => {
   const [visible, setVisible] = useState(false)
 
   return (
-    <aside>
+    <>
       <button onClick={() => setVisible(!visible)} className='links-toggle'>
         toggle site navigation
       </button>
@@ -62,25 +57,6 @@ export const Navigation = () => {
           </React.Fragment>
         )}
       </nav>
-      <nav className='links-social'>
-        <h4>Connect</h4>
-        <ul className='links-connect'>
-          {
-            Object.entries({
-              Twitter: ['https://twitter.com/_jnf', twitterIcon],
-              Github: ['https://www.github.com/jnf/', githubIcon],
-              Discogs: ['https://www.discogs.com/user/jnf', discogsIcon],
-              LinkedIn: ['https://www.linkedin.com/in/-jnf/', linkedinIcon],
-            }).map(([service, [href, icon]]) =>
-              <li key={`link-${service}`}>
-                <a className='links-connect__link' target='_blank' rel='noopener noreferrer' href={href}>
-                  <img className='links-connect__icon' src={icon} alt={`icon for ${service}`} />
-                </a>
-              </li>
-            )
-          }
-        </ul>
-      </nav>
-    </aside>
+    </>
   )
 }
