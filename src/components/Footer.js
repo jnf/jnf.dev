e2a9fd901171aedf2c8bdc4d62b17648d4459080
jnf <code@jnf.dev>
2021-04-01 08:22:49 -0700

    finishes the refactor of Footer by making the style content and naming consitent with other page components and adding the footer to the overall grid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,21 @@ import React from 'react'
 import { OffsiteLink } from 'components/OffsiteLink'
 
 export const Footer = () =>
-  <footer className='license-content'>
-    This content by <a href='https://jnf.dev'>jnf</a> is licensed under a {' '}
-    <OffsiteLink href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
-      Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
-    </OffsiteLink>.
-    <OffsiteLink href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
-      <img
-        alt='Creative Commons License'
-        src='https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png'
-      />
-    </OffsiteLink>
-    All other rights reserved. © {new Date().getFullYear()}
-    <br />
-    Built with <OffsiteLink href='https://www.gatsbyjs.org'>Gatsby</OffsiteLink>.
-    Hosted on <OffsiteLink href='https://www.netlify.com/'>Netlify</OffsiteLink>.
-    Source code available on <OffsiteLink href='https://github.com/jnf/jnf.dev'>Github</OffsiteLink>.
+  <footer className='site-footer'>
+    <p className='site-footer--licensing'>
+      This content by <a href='https://jnf.dev'>jnf</a> is licensed under a {' '}
+      <OffsiteLink href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
+        Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+        <img
+          alt='Creative Commons License'
+          src='https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png'
+        />
+      </OffsiteLink>
+    </p>
+    <p>
+      All other rights reserved. © {`${new Date().getFullYear()} `}
+      Built with <OffsiteLink href='https://www.gatsbyjs.org'>Gatsby</OffsiteLink>.
+      Hosted on <OffsiteLink href='https://www.netlify.com/'>Netlify</OffsiteLink>.
+      Source code available on <OffsiteLink href='https://github.com/jnf/jnf.dev'>Github</OffsiteLink>.
+    </p>
   </footer>
