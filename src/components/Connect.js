420b2247e02abf2327ab1e97021ef0f43d2bbed7
jnf <hi@jnf.dev>
2021-07-10 10:43:43 -0700

    adds bandcamp to the social connection links

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -3,17 +3,19 @@ import twitterIcon from '../images/twitter.svg'
 import githubIcon from '../images/github.svg'
 import linkedinIcon from '../images/linkedin.svg'
 import discogsIcon from '../images/discogs.svg'
+import bandcampIcon from '../images/bandcamp.svg'
 
 export const Connect = () =>
   <nav className='site-sidebar--header__connect'>
     <ul className='links-connect'>
       {
-        Object.entries({
-          Twitter: ['https://twitter.com/_jnf', twitterIcon],
-          Github: ['https://www.github.com/jnf/', githubIcon],
-          Discogs: ['https://www.discogs.com/user/jnf', discogsIcon],
-          LinkedIn: ['https://www.linkedin.com/in/-jnf/', linkedinIcon],
-        }).map(([service, [href, icon]]) =>
+        [
+          ['Twitter', 'https://twitter.com/_jnf', twitterIcon],
+          ['Bandcamp', 'https://bandcamp.com/jnf', bandcampIcon],
+          ['Github', 'https://www.github.com/jnf/', githubIcon],
+          ['Discogs', 'https://www.discogs.com/user/jnf', discogsIcon],
+          ['LinkedIn', 'https://www.linkedin.com/in/-jnf/', linkedinIcon],
+        ].map(([service, href, icon]) =>
           <li key={`link-${service}`}>
             <a className='links-connect__link' target='_blank' rel='noopener noreferrer' href={href}>
               <img className='links-connect__icon' src={icon} alt={`icon for ${service}`} />
