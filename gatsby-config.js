8240763af99376ca8854ccb63286e5b3c4269870
jnf <code@jnf.dev>
2022-03-27 13:30:16 -0700

    trying to please the netlify robots

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+const { join } = require('path')
 module.exports = {
   siteMetadata: {
     title: 'jnf.dev',
@@ -12,11 +13,18 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/images`,
+        path: join(__dirname, 'src', 'images'),
       },
     },
     'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        defaults: {
+          formats: ['auto', 'webp'],
+        }
+      }
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -27,7 +35,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/content`,
+        path: join(__dirname, 'src', 'content'),
       },
     },
     {
